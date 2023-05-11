2283f732691a9cce1d89a659da5ef79021ac2c22
Saumyasingh28 <ss899426@gmail.com>
2023-05-11 14:26:18 +0200

    added eslint-disable-next-line react-hooks/exhaustive-deps

--- src/components/BingoCard.jsx
+++ src/components/BingoCard.jsx
@@ -24,6 +24,7 @@ export default function BingoCard({
     if (newbingoCount !== oldBingoCount) {
       setOldBingoCount(newbingoCount);
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [cardArray]);
   return (
     <>
